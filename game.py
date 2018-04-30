232ebe99b59179192aacf841fa6a29a6519f6f95
xususan <susan.xu97@gmail.com>
2018-04-30 02:27:42 -0400

    moving board

--- game.py
+++ game.py
@@ -1,5 +1,12 @@
 import random
+import curses
+import copy
+import pdb
 
+UP = 'w'
+DOWN = 's'
+LEFT = 'a'
+RIGHT = 'd'
 class Game:
     def __init__(self, board):
         self.wall = 'O'
@@ -8,6 +15,25 @@ class Game:
         self.num_rows = len(board)
         self.num_cols = len(board[0])
         self.board = board
+        self.players = {}
+
+    def pack_board(self, username):
+        string_of_board = []
+
+        def print_square(x):
+            try:
+                if x.username == username:
+                    return 'X' # print an X for the current player
+            except:
+                return str(x)
+
+        for row in self.board:
+            str_of_row = [print_square(x) if x != '\n' else '' for x in row]
+            string_row = ''.join(str_of_row)
+            string_of_board.append(string_row)
+
+        final_string = ','.join(string_of_board)
+        return final_string.encode('utf-8')
 
     def print_board(self):
         for row in self.board:
@@ -18,24 +44,98 @@ class Game:
         while True:
             r = random.randint(0, self.num_rows - 1)
             c = random.randint(0, self.num_cols - 1)
-            if self.board[r][c] != self.wall:
+            if self.board[r][c] == self.empty:
                 return (r, c)
 
-    def spawn_player(self):
+    def spawn_player(self, username):
         r, c = self.random_empty_location()
-        self.board[r][c] = self.player
+        new_player = Player(username, (r,c))
+        self.players[username] = new_player
+        self.board[r][c] = new_player
 
     def start_game(self):
         self.spawn_player()
-
         self.print_board()
 
-def main():
+    def is_valid(self, player, move):
+        row, col = player.next(move)
+
+        # Check if out of bounds
+        if (col >= self.num_cols or col < 0 or row >= self.num_rows or row < 0):
+            return False
+
+        # Check if wall
+        if self.board[row][col] == 'O':
+            return False
+
+        return True
+
+
+    def update_state(self, player, move):
+        if self.is_valid(player, move):
+            # update the game board
+            prev_r, prev_c = player.position
+
+            new_r, new_c = player.next(move)
+
+            self.board[prev_r][prev_c] = self.empty
+            self.board[new_r][new_c] = player
+
+            # move the player's position 
+            player.move(move)
+        else:
+            # probably raise a custom exception
+            pass
+
+class Player:
+    def __init__(self, username, pos):
+        self.username = username
+        self.position = pos
+
+    def set_position(self, pos):
+        self.position = pos
+
+    def next(self, direction):
+        x, y = self.position # # most definitely bad design lol fix this later
+        if direction == UP:
+            next_square = (x, y + 1)
+        elif direction == DOWN:
+            next_square = (x, y - 1)
+        elif direction == LEFT:
+            next_square = (x - 1, y)
+        elif direction == RIGHT:
+            next_square = (x + 1, y)
+        return next_square
+
+    def move(self, direction):
+        self.position = self.next(direction) # validation is done by the game idk if that's good
+
+
+    def __str__(self):
+        return self.username[0]
+
+
+# def run_screen(stdscr):
+#     # Clear screen
+#     stdscr.clear()
+#     # Proceed with your program
+#     stdscr.refresh()
+#     stdscr.getkey()
+#     print("Running some program")
+
+# def main():
+#     curses.wrapper(run_screen)
+
+def load_new_game():
     board = []
     with open('board.txt', 'r') as f:
         for line in f:
             board.append(list(line))
     game = Game(board)
+    return game
+
+def main():
+    game = load_new_game()
     game.start_game()
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -2,10 +2,27 @@ import socket
 import sys
 import _thread as thread
 from struct import *
+import game
+import server_receive
+
+
+version = b'\x01'
+
+header_fmt = '!cIc'
+header_len = calcsize(header_fmt)
+
+opcode_to_function = {
+    b'\x01': server_receive.create_request,
+    b'\x02': server_receive.move_request,
+    # b'\x03': server_receive.list_accounts_request,
+    # b'\x04': server_receive.send_message_request,
+    # b'\x05': server_receive.get_messages_request,
+    # b'\x06': server_receive.delete_request,
+    # b'\x07': server_receive.logout_request,
+}
 
 def handler(conn, lock, shared_data):
     while True:
-        print('hello', conn)
         try:
             print('trying to recv...')
             msg = conn.recv(1024)
@@ -15,29 +32,30 @@ def handler(conn, lock, shared_data):
             thread.exit()
 
         print('received', msg)
+        print('game state', shared_data['game'])
 
-        if len(msg) == 0:
+        if len(msg) == 0: # Disconnect signal
             thread.exit()
 
+        header = unpack(header_fmt, msg[:header_len])
+        body_packed = msg[header_len:]
 
-        # header = unpack(header_fmt, msg[:header_len])
-        # body_packed = msg[header_len:]
+        # Check that versions match up and check integrity of message
+        # If not, drop the connection because there's nothing better to do.
+        if header[0] != version or header[1] != len(msg) - header_len: 
+            thread.exit()
 
-        # # Check that versions match up and check integrity of message
-        # # If not, drop the connection because there's nothing better to do.
-        # if header[0] != version or header[1] != len(msg) - header_len:
-            # thread.exit()
-        # opcode = header[2]
+        opcode = header[2]
+        with lock:
+            opcode_to_function[opcode](conn, body_packed, shared_data)
 
-        # with lock:
-            # opcode_to_function[opcode](conn, im_conn, body_packed, shared_data)
 
 def main():
     if(len(sys.argv) != 3):
         print("Usage 'python server.py <host> <port>'. Example: python server.py 10.252.215.26 8090")
         sys.exit()
 
-    shared_data = {}
+    shared_data = {'game':game.load_new_game()}
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
     host = str(sys.argv[1])
